94b46e9cffba882ffb4e540b636b16d50f8d0a15
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-05-29 15:35:19 +0530

    3 styles of same task: execution time tested

---
+++ check_execution_time.py
@@ -0,0 +1,9 @@
+'''
+timeit module allows user to measure the execution time of small Python code snippets
+'''
+
+import timeit
+
+print(timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
+print(timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000))
+print(timeit.timeit('"-".join(map(str, range(100)))', number=10000))
