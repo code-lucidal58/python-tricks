4f7ce97a5daede4f8203118faeea0613c1908f10
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-07-21 18:44:50 +0530

    list and set tricks

--- list_tricks.py
+++ list_tricks.py
@@ -1,6 +1,6 @@
-'''
+"""
 List slicing
-'''
+"""
 
 # clear all elements from a list:
 lst = [1, 2, 3, 4, 5]
@@ -24,3 +24,17 @@ List comprehension
 '''
 even_squares = [x * x for x in range(10) if not x % 2]
 print(even_squares)  # [0, 4, 16, 36, 64]
+
+"""
+ways of checking if all items in a list are equal:
+"""
+
+lst1 = ['a', 'a', 'a']
+
+# Most efficient
+print(len(set(lst)) == 1)
+print(all(x == lst[0] for x in lst))
+print(lst.count(lst[0]) == len(lst))
+# Least efficient
+
+# constructing a set is less efficient memory and speed-wise
