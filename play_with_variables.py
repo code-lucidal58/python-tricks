613f42abeaa10ca70f296621075f88a43a7ee28b
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-07-22 13:33:48 +0530

    __str__ and __repr__

--- play_with_variables.py
+++ play_with_variables.py
@@ -1,17 +1,32 @@
-# Why Python Is Great:
-# In-place value swapping
+import datetime
 
-# Let's say we want to swap
-# the values of a and b...
+"""
+In-place value swapping
+"""
+# swapping the values of a and b
 a = 23
 b = 42
 
-# The "classic" way to do it
-# with a temporary variable:
+# The "classic" way to do it with a temporary variable:
 tmp = a
 a = b
 b = tmp
 
-# Python also lets us
-# use this short-hand:
+# Python also lets us use this
 a, b = b, a
+
+"""
+When To Use __repr__ vs __str__?
+"""
+# Emulate what the std lib does:hand
+
+today = datetime.date.today()
+
+# Result of __str__ should be readable:
+print(str(today))
+
+# Result of __repr__ should be unambiguous:
+print(repr(today))
+
+# Python interpreter sessions use__repr__ to inspect objects:
+print(today)
