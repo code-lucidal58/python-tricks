e76a5cc5428a335b51f1c18976cd67c1dac6d2b5
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-09-03 13:54:56 +0530

    dictionary tricks appended

--- dictionary_tricks.py
+++ dictionary_tricks.py
@@ -1,6 +1,22 @@
-# Merging two dictionaries
+# merging two dictionaries
 
 x = {"a": 1, "b": 2, "c": 3}
 y = {"d": 4, "e": 5}
 z = {**x, **y}
 print("The merged dictionary is", z)
+
+# sort a python dict by value
+# order os dictionary cannot be fixed nut conserving the data type
+# hence sorting is done using dictionary values and converted to a list where each item is a tuple
+xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
+xs = sorted(xs.items(), key=lambda p: p[1])  # xs is now a list where each dictionary entry is a tuple
+print("Sorted dictionary values using lambda", xs)
+# [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+# OR
+import operator
+
+xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
+xs = sorted(xs.items(), key=operator.itemgetter(1))
+print("Sorted dictionary values using operator.itemgetter", xs)
+# [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
